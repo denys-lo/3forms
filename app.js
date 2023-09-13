07c6a2747e34813d317507bc9b832be3ad7c341d
denys-lo <div.p.div@gmail.com>
2023-09-13 21:43:19 +0200

    Add handlebars beta

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express"); // подключение express
 const cors = require("cors");
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
 const verbs = require("./verbs");
 
 const app = express(); // создаем объект приложения
 app.use(cors({ origin: '*' })); // origin: '*' разрешение для cors на все подключения
-app.use(express.static(__dirname + "/public"));
-// app.use("/irregular-verbs/", express.static(__dirname + "/public"));
+app.engine("hbs", expressHbs.engine({
+  layoutsDir: "views/layouts",
+  defaultLayout: "layout",
+  extname: "hbs"
+}));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+// app.use(express.static(__dirname + "/public"));
+
+app.get("/", (request, response) => {
+  response.render("alphabet.hbs");
+});
 
 app.get("/irregular-verbs/", (request, response) => {
   response.json(verbs);
@@ -21,7 +33,8 @@ app.get("/irregular-verbs/:verb", (request, response) => {
       data.pastSimple.slice(data.pastSimple.indexOf("/") + 1) === verb ||
       data.pastParticiple === verb
   );
-  response.json(result);
+  // response.json(result);
+  response.render("verb.hbs", result[0]);
 });
 
 app.listen(3000);
